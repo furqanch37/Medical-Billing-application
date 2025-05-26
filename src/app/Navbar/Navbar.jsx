b1d8d95a538dd2f5e85c944ddc4a61a296aa822b
Muhammad Furqan <muhammadfurqanch517@gmail.com>
2025-05-26 14:27:47 +0500

    commit

--- src/app/Navbar/Navbar.jsx
+++ src/app/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './mainnavbar.css';
-import { FaSearch } from 'react-icons/fa';
+import { FaSearch, FaBars, FaTimes } from 'react-icons/fa';
 
 const MainNavbar = ({ scrolled }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
   return (
     <div className={`main-navbar ${scrolled ? "scrolled-bg" : ""}`}>
       <div className="logo">
@@ -10,22 +14,25 @@ const MainNavbar = ({ scrolled }) => {
         <span className="brand"><strong>Care</strong>Watch</span>
       </div>
 
-    <div className='flexed-div'>
-        <div className="nav-links">
-        <a href="#">Home</a>
-        <a href="#">Departments</a>
-        <a href="#">Timetable</a>
-        <a href="#">Features</a>
-        <a href="#">Shortcodes</a>
-        <a href="#">Post Types</a>
+      {/* Burger Icon */}
+      <div className="burger-icon" onClick={toggleMenu}>
+        {menuOpen ? <FaTimes /> : <FaBars />}
       </div>
 
-      <div className="search">
-        <button>
-          <FaSearch />
-        </button>
+      <div className={`flexed-div ${menuOpen ? "show-menu" : ""}`}>
+        <div className="nav-links">
+          <a href="#">Home</a>
+          <a href="#">Departments</a>
+          <a href="#">Timetable</a>
+          <a href="#">Features</a>
+          <a href="#">Shortcodes</a>
+          <a href="#">Post Types</a>
+        </div>
+
+        <div className="search">
+          <button><FaSearch /></button>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
